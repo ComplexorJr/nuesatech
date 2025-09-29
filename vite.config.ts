b1ce6a7186ae6d26baffc669af7151508bc22ad6
ComplexorJr <oluwakoredepeter@gmail.com>
2025-09-29 04:54:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react({
     jsxRuntime: 'automatic'
   })],
-  base: basePath, // ✅ Correct base handling
+  base: "/", // ✅ Correct base handling
 
   resolve: {
     alias: {
